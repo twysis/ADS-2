9635278d888a6e3ba09b75902b1c580668da885a
Матвей Головенко <91070065+twysis@users.noreply.github.com>
2022-02-27 17:18:27 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,53 +4,53 @@
 
 
 double pown(double value, uint16_t n) {
-  double result = 1;
-	for (int i = 0; i < n; i++) {
-		result *= value;
-	}
-	return result;
+   double result = 1;
+      for (int i = 0; i < n; i++) {
+         result *= value;
+      }
+      return result;
 }
 
 uint64_t fact(uint16_t n) {
-  if (n == 0) {
-	  return 1;
-  } else {
-	  return n * fact(n - 1);
-  }
+   if (n == 0) {
+      return 1;
+   } else {
+      return n * fact(n - 1);
+   }
 }
 
 double calcItem(double x, uint16_t n) {
-  return (pown(x, n)) / (fact(n));
+   return (pown(x, n)) / (fact(n));
 }
 
 double expn(double x, uint16_t count) {
-	double result = 1;
-	for (int i = 1; i <= count; i++) {
-		result += calcItem(x, i);
-	}
-	return result;
+   double result = 1;
+      for (int i = 1; i <= count; i++) {
+         result += calcItem(x, i);
+      }
+      return result;
 }
 
 double sinn(double x, uint16_t count) {
-	double result = x;
-	for (int i = 2; i <= count; i++) {
-		if (pown(-1, i - 1) == 1) {
-			result += calcItem(x, (i * 2) - 1);
-		} else {
-			result -= calcItem(x, (i * 2) - 1);
-		} 
-	}
-	return result;
+   double result = x;
+      for (int i = 2; i <= count; i++) {
+         if (pown(-1, i - 1) == 1) {
+            result += calcItem(x, (i * 2) - 1);
+         } else {
+            result -= calcItem(x, (i * 2) - 1);
+         } 
+      }
+      return result;
 }
 
 double cosn(double x, uint16_t count) {
-	double result = 1;
-	for (int i = 2; i <= count; i++) {
-		if (pown(-1, i - 1) == 1) {
-			result += calcItem(x, (i * 2) - 2);
-		} else {
-			result -= calcItem(x, (i * 2) - 2);
-		}
-	}
-	return result;
+   double result = 1;
+      for (int i = 2; i <= count; i++) {
+         if (pown(-1, i - 1) == 1) {
+            result += calcItem(x, (i * 2) - 2);
+         } else {
+            result -= calcItem(x, (i * 2) - 2);
+         }
+      }
+      return result;
 }
